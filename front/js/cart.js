225950c738e8d76881d3dfa4354418832956f675
NxxhL <89213023+chifouu65@users.noreply.github.com>
2022-11-10 15:09:47 +0100

    .

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,5 +1,4 @@
 const storage = JSON.parse(localStorage.getItem('orders'));
-console.log(storage);
 
 //request data from api
 async function getProductData(id) {
@@ -13,9 +12,10 @@ async function getProductData(id) {
 
 //control page
 (function load() {
-    if (storage.length === 0) {
+    console.log(storage);
+    if (storage == null || storage.length === 0) {
         empty().catch((err) => console.log(err));
-    } else {
+    } else if (storage !== 0) {
         //si il n'est pas vide
         displayCart().catch((err) => console.log(err));
         total().catch((err) => console.log(err));
@@ -45,10 +45,8 @@ async function total() {
 
 //create empty cart
 async function empty() {
-    if (storage.length === 0) {
-        document.querySelector('#empty').innerHTML = `<p>Votre panier est vide :( <br/><a href="index.html">Home</a></p>`;
-        document.querySelector('.cart').hidden = true;
-    }
+    document.querySelector('#empty').innerHTML = `<p>Votre panier est vide :( <br/><a href="index.html">Home</a></p>`;
+    document.querySelector('.cart').hidden = true;
 }
 
 async function displayCart() {
@@ -107,7 +105,6 @@ async function removeProductInCart() {
                     console.log(storage[i]);
                     storage.splice(i, 1);
                     localStorage.setItem('orders', JSON.stringify(storage));
-                    console.log(storage);
                     const article = e.target.parentNode.parentNode.parentNode.parentNode;
                     article.remove();
                     total().catch((err) => console.log(err));
@@ -129,7 +126,6 @@ async function updateQuantity() {
             const id = e.target.parentNode.parentNode.parentNode.parentNode.dataset.id;
             const color = e.target.parentNode.parentNode.parentNode.parentNode.dataset.color;
             const qty = e.target.value;
-            console.log(id, color, qty);
             for (let i = 0; i < storage.length; i++) {
                 //if id & color are the same in storage & input => update quantity in storage
                 if (storage[i].id === id && storage[i].colors === color) {
@@ -148,3 +144,144 @@ async function updateQuantity() {
     }
 }
 
+function validateForm() {
+    const formQuestion = document.querySelectorAll('.cart__order__form__question input');
+    let valid = false;
+
+    formQuestion.forEach((input) => {
+        validation(input);
+
+        input.addEventListener('input', () => {
+            validation(input);
+        })
+    })
+    for (let form of formQuestion) {
+        console.log(form);
+        valid = !(form.value === "" || form.classList.contains('error'));
+        if (!valid) {
+            break;
+        }
+    }
+    console.log(valid);
+
+    return valid;
+}
+
+function validation(input) {
+    const regexFirstName = new RegExp(/^([a-zA-ZÀ-ÿ]{2,26})(-[a-zA-ZÀ-ÿ]{2,26})?(\s[a-zA-ZÀ-ÿ]{2,26})?$/, 'g')
+    const regexLastName = new RegExp(/^([a-zA-ZÀ-ÿ]{2,26})(-[a-zA-ZÀ-ÿ]{2,26})?(\s[a-zA-ZÀ-ÿ]{2,26})?$/, 'g')
+    const regexAddress = new RegExp(/^([a-zA-ZÀ-ÿ0-9]{2,26})(-[a-zA-ZÀ-ÿ0-9]{2,26})?(\s[a-zA-ZÀ-ÿ0-9]{2,26})?$/, 'g')
+    const regexCity = new RegExp(/^([a-zA-ZÀ-ÿ]{2,26})(-[a-zA-ZÀ-ÿ]{2,26})?(\s[a-zA-ZÀ-ÿ]{2,26})?$/, 'g')
+    const regexEmail = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'g')
+
+    const err = input.nextElementSibling
+
+    if (input.name === 'firstName') {
+        if (regexFirstName.test(input.value)) {
+            input.classList.remove('error');
+            err.classList.remove('error');
+        } else {
+            input.classList.add('error');
+            err.classList.add('error');
+        }
+    } else if (input.name === 'lastName') {
+        if (regexLastName.test(input.value)) {
+            input.classList.remove('error');
+            err.classList.remove('error');
+        } else {
+            input.classList.add('error');
+            err.classList.add('error');
+        }
+    } else if (input.name === 'address') {
+        if (regexAddress.test(input.value)) {
+            input.classList.remove('error');
+            err.classList.remove('error');
+        } else {
+            input.classList.add('error');
+            err.classList.add('error');
+        }
+    } else if (input.name === 'city') {
+        if (regexCity.test(input.value)) {
+            input.classList.remove('error');
+            err.classList.remove('error');
+        } else {
+            input.classList.add('error');
+            err.classList.add('error');
+        }
+    } else if (input.name === 'email') {
+        if (regexEmail.test(input.value)) {
+            input.classList.remove('error');
+            err.classList.remove('error');
+            console.log('ok');
+        } else {
+            console.log('form pas ok');
+            input.classList.add('error');
+            err.classList.add('error');
+        }
+    } else {
+        input.classList.add('error');
+        err.classList.add('error');
+    }
+}
+
+function getLocalStorage() {
+    let ordersInLocalStorage = localStorage.getItem("orders")
+
+    return ordersInLocalStorage != null ? JSON.parse(ordersInLocalStorage) : []
+}
+
+function sendDataOrder(data) {
+    return fetch('http://localhost:3000/api/products/order', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: data
+    }).then(res => {
+        if (res.ok) {
+            return res.json();
+        }
+    }).catch(err => console.log(err));
+}
+
+(async function sendOrder() {
+    const submitButton = document.getElementById('order');
+    const contactForm = document.querySelector('.cart__order__form')
+    submitButton.addEventListener('click', () => {
+        validateForm();
+    })
+
+    contactForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        if (validateForm()) {
+            const contact = {
+                firstName: document.getElementById('firstName').value,
+                lastName: document.getElementById('lastName').value,
+                address: document.getElementById('address').value,
+                city: document.getElementById('city').value,
+                email: document.getElementById('email').value
+            }
+            const products = getLocalStorage();
+
+            if (products.length > 0) {
+                let cart = products.map((product) => product.id);
+                const dataToSend = JSON.stringify({
+                    "contact": contact,
+                    "products": cart
+                })
+                const response = await sendDataOrder(dataToSend);
+                console.log(response);
+
+                if (response) {
+                    localStorage.clear();
+                    window.location.href = `confirmation.html?orderId=${response.orderId}`;
+                }
+
+            } else {
+                alert("Votre panier est vide");
+            }
+        }
+    })
+
+})();
---
+++ front/js/confirmation.js
@@ -0,0 +1,21 @@
+function urlOrderParams() {
+    // Get the order ID from the URL
+    let url = new URL(window.location.href);
+    let params = new URLSearchParams(url.search);
+
+    //if there is an order ID in the URL, return it
+    if (params.get('orderId')) {
+        const orderId = params.get('orderId');
+        return orderId;
+    } else {
+        alert('Order Id not found');
+    }
+}
+
+// Get the order ID from the URL and display order in the HTML
+(async function load() {
+    const order = urlOrderParams();
+
+    document.querySelector('#orderId').textContent = order;
+    localStorage.clear();
+})();
